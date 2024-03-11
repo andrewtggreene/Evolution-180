ef660b2ff3522b6511a45a2c6ac2b486074393e5
andrewtggreene <mathtron@gmail.com>
2024-03-10 19:02:17 -0600

    Finished two classes

--- Sandbox/LifeForm.cs
+++ Sandbox/LifeForm.cs
@@ -1,3 +1,48 @@
+using System.Runtime.Versioning;
+
 class LifeForm{
-    
+    bool _advanced;
+    bool _friendly;
+    bool _potentialFood;
+    List<Resource> _neededResources;
+    List<Skill> _skills;
+    bool _spaceTravel;
+    Planet _currentPlanet;
+    SolarSystem _SolarSystem;
+    string _name;
+    string _description;
+    public LifeForm(string name, string description, bool advanced, bool friendly, bool potentialFood, bool spaceTravel, Planet planet, SolarSystem solarSystem, List<Skill> skills, List<Resource> resources){
+        _name = name;
+        _description = description;
+        _advanced = advanced;
+        _friendly = friendly;
+        _potentialFood = potentialFood;
+        _spaceTravel = spaceTravel;
+        _currentPlanet = planet;
+        _SolarSystem = solarSystem;
+        _skills = skills;
+        _neededResources = resources;
+    }
+    public string GetName(){
+        return _name;
+    }
+    public string GetDescription(){
+        return _description;
+    }
+    public List<bool> GetAttributes(){
+        List<bool> attributes = new List<bool>{_advanced, _friendly, _potentialFood, _spaceTravel};
+        return attributes;
+    }
+    public Planet GetPlanet(){
+        return _currentPlanet;
+    }
+    public SolarSystem GetSolarSystem(){
+        return _SolarSystem;
+    }
+    public List<Skill> GetSkills(){
+        return _skills;
+    }
+    public List<Resource> GetResources(){
+        return _neededResources;
+    }
 }
--- Sandbox/User.cs
+++ Sandbox/User.cs
@@ -5,7 +5,6 @@ class User{
     protected List<TechUpgrades> _techUpgrades = new List<TechUpgrades>{};
     protected int _money;
     protected List<SpaceCraft> _spaceShips = new List<SpaceCraft>{};
-    protected List<OutPost> _outPosts = new List<OutPost>{};
     protected List<SpaceStation> _spaceStations = new List<SpaceStation>{};
     protected SolarSystem _currentSolarSystem;
     protected List<LifeForm> _foundLifeForms = new List<LifeForm>{};
@@ -17,5 +16,78 @@ class User{
         _position = position;
         _currentSolarSystem = solarSystem;
     }
-
+    public void SetName(string name){
+        _name = name;
+    }
+    public string GetName(){
+        return _name;
+    }
+    public void SetPosition(Planet planet){
+        _position = planet;
+    }
+    public Planet GetPlanet(){
+        return _position;
+    }
+    public void SetLocale(SolarSystem solarSystem){
+        _currentSolarSystem = solarSystem;
+    }
+    public SolarSystem GetLocale(){
+        return _currentSolarSystem;
+    }
+    public void BuySpaceship(int cost, SpaceCraft spaceCraft){
+        _money -= cost;
+        _spaceShips.Add(spaceCraft);
+    }
+    public List<SpaceCraft> GetListSpaceCraft(){
+        return _spaceShips;
+    }
+    public void BuyTechUpgrade(int cost, TechUpgrades techUpgrades){
+        _money -= cost;
+        _techUpgrades.Add(techUpgrades);
+    }
+    public List<TechUpgrades> GetListTech(){
+        return _techUpgrades;
+    }
+    public void BuySkill(int cost, Skill skill){
+        _money -= cost;
+        _abilities.Add(skill);
+    }
+    public List<Skill> GetListAbilities(){
+        return _abilities;
+    }
+    public void LaunchSpaceStation(int cost, SpaceStation spaceStation){
+        _money -= cost;
+        _spaceStations.Add(spaceStation);
+    }
+    public List<SpaceStation> GetListSpaceStations(){
+        return _spaceStations;
+    }
+    public void AddBase(int cost, Base based){
+        _money -= cost;
+        _bases.Add(based);
+    }
+    public List<Base> GetListBases(){
+        return _bases;
+    }
+    public void AddOutpost(int cost, OutPost outPost){
+        _money -= cost;
+        _bases.Add(outPost);
+    }
+    public void UpgaradToBase(int cost, int index, Base newBase){
+        _money -= cost;
+        _bases.RemoveAt(index);
+        _bases.Add(newBase);
+    }
+    public void FindLife(LifeForm lifeForm){
+        _foundLifeForms.Add(lifeForm);
+    }
+    public List<LifeForm> GetListLifeForms(){
+        return _foundLifeForms;
+    }
+    public void AddMoney(int money){
+        _money += money;
+    }
+    public int GetMoney(){
+        return _money;
+    }
 }
